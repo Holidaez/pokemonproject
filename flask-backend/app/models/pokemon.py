2e987de362c6d960050d539e894304ab88589db0
holidaez <holidaez@DESKTOP-GG36629.localdomain>
2022-12-08 16:53:55 -0500

    Setup-Completed

--- flask-backend/app/models/pokemon.py
+++ flask-backend/app/models/pokemon.py
@@ -1,12 +1,13 @@
-from .models import db
+from . import db
 class Pokemon(db.Model):
-    number=db.Column(db.Integer, nullable=False, unique:True, NumberRange(min=1,max=None))
+    __tablename__ = 'pokemon'
+    number=db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
     attack=db.Column(db.Integer,nullable=False)
     defense=db.Column(db.Integer, nullable=False)
     image_url=db.Column(db.String(255), nullable=False)
     name=db.Column(db.String(255),nullable=False)
     type=db.Column(db.String(255),nullable=False)
     moves=db.Column(db.String(255),nullable=True) #Needs updating
-    encounter_rate=db.Column(db.Float, nullable=False)
+    encounter_rate=db.Column(db.Float(precision=2,asdecimal=True), nullable=False)
     catch_rate=db.Column(db.Float(precision=2,asdecimal=True), nullable=False)
     captured=db.Column(db.Boolean, nullable=False)
